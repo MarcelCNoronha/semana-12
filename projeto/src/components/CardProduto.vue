75709e875837dea970997f3a0248892f7e21e5a2
Marcel Cardoso de Noronha <95543728+MarcelCNoronha@users.noreply.github.com>
2023-08-22 11:57:03 -0300

    feat - exercicio 2

--- projeto/src/components/CardProduto.vue
+++ projeto/src/components/CardProduto.vue
@@ -1,10 +1,18 @@
 <template>
-    <v-card>
-
+    <v-card elevation="5" class="ma-3" width="400">
+      <v-card-text class="d-flex flex-column">
+       
+        <v-img width="200" :src="imagem"></v-img>
+        <span>{{ nome }}</span>
+        <span><b>{{ "10x de R$" + parcela }}</b></span>
+        <span>{{ "R$" + preco }}</span>
+      </v-card-text>
     </v-card>
-</template>
+  </template>
 <script>
+export default {
+  props: ["id", "imagem", "nome", "preco", "parcela"],
+};
 </script>
 
-<style>
-</style>
+<style></style>
--- projeto/src/views/Home.vue
+++ projeto/src/views/Home.vue
@@ -1,8 +1,55 @@
 <template>
-<div>Start</div>
+      <div class="ma-5 d-flex justify-start">
+      <h2>Produtos</h2>
+    </div>
+  <div class="d-flex mx-5 flex-row">
+    <CardProduto
+      v-for="produto in listaProdutos" 
+      :key="produto.id" 
+      :id="produto.id" 
+      :imagem="produto.imagem"
+      :nome="produto.nome" 
+      :preco="produto.preco" 
+      :parcela="produto.parcela" 
+    />
+  </div>
 </template>
   
   <script>
+  import CardProduto from "../components/CardProduto.vue"
+  import axios from 'axios'
+  export default {
+    components: {
+      CardProduto
+    },
+    data() {
+      return {
+        listaProdutos: []
+      }
+    },
+    methods: {
+      async AdicionarProduto(id) {
+        // editar o medicamento e marcar como favorito
+        // o filter retorna sempre um array, por isso vamos sempre usar a posição 0
+        let produto = this.listaProdutos.filter(item => item.id == id)
+  
+        if(!!produto[0]){
+          try {
+            produto[0].carrinho = !produto[0].favorito
+  
+            var result = await axios.put(`http://localhost:50001/produtos/${id}`, produto[0])
+          } catch (erro) {
+            console.log(erro)
+          }
+        }
+      }
+    },
+    mounted(){
+      axios.get('http://localhost:50001/produtos')
+      .then(res => this.listaProdutos = res.data)
+      .catch(erro => console.log(erro))
+    }
+  }
   </script>
   
   <style scoped>
